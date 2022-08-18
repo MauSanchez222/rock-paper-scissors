e852bc2ff6cf5e211d175cc7e8ca84be67a8f51c
Mauri <mauricioasan96@gmail.com>
2022-08-18 19:39:09 -0300

    added scripting to play RPS through a UI

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let computerSelection;
 let playerSelection;
 let playerWins = 0;
 let computerWins = 0;
-let tiedGames = 0;
 
 function getComputerChoice(choices){
     return choices[Math.floor(Math.random() * choices.length)];
@@ -12,55 +11,106 @@ function getComputerChoice(choices){
 function playRound(playerSelection, computerSelection){
     if (playerSelection === 'rock'){
         switch (computerSelection){
-            case 'paper': computerWins++;
-                          return 'You lose';
-                          break;
-            case 'scissors': playerWins++;
-                             return 'You win';
-                             break;
-            default:      tiedGames++;
-                          return 'It\'s a tie'
+            case 'paper': 
+                computerWins++;
+                break;
+            case 'scissors': 
+                playerWins++;
+                break;
+            default:      
+                break;
         }
     }
     else if (playerSelection === 'paper'){
         switch (computerSelection){
-            case 'scissors': computerWins++;
-                             return 'You lose';
-                             break;
-            case 'rock': playerWins++;
-                         return 'You win';
-                         break;
-            default:     tiedGames++;
-                         return 'It\'s a tie'
+            case 'scissors': 
+                computerWins++;
+                break;
+            case 'rock': 
+                playerWins++;
+                break;
+            default:     
         }
     }
         else{
             switch (computerSelection){
-                case 'rock': computerWins++;
-                             return 'You lose';
-                             break;
-                case 'paper': playerWins++; 
-                              return 'You win';
-                              break;
-                default:     tiedGames++;
-                             return 'It\'s a tie'
+                case 'rock': 
+                    computerWins++;
+                    break;
+                case 'paper': 
+                    playerWins++; 
+                    break;
+                default:     
+                    tiedGames++;
             }
         }
     }
-    
-function showResult(){
-    console.log('Your choice: ' + playerSelection + ' Computer\'s choice: ' + computerSelection);
-    console.log('Score: Player ' + playerWins + ' Computer ' + computerWins + ' Ties: ' + tiedGames)
+
+
+function showScores(){
+    document.getElementById('pscore').innerHTML = playerWins;
+    document.getElementById('oscore').innerHTML = computerWins;
 }
 
-function game(){
-    for (let games = 5; games > 0; games--){
-        playerSelection = prompt('What will your play be?').toLowerCase();
-        computerSelection = getComputerChoice(choices);
-        playRound(playerSelection,computerSelection);
-        showResult();
+function opponentsPlay(computerSelection){
+    switch(computerSelection){
+        case 'rock':
+            document.getElementById('opponent-played').innerHTML = '<img src="./images/rock.jpg">';
+            break;
+        case 'paper':
+            document.getElementById('opponent-played').innerHTML = '<img src="./images/paper.jpg">';
+            break;
+        case 'scissors':
+            document.getElementById('opponent-played').innerHTML = '<img src="./images/scissors.jpg">';
+            break;
     }
-    playerWins > computerWins ? console.log('You win') : computerWins > playerWins ? console.log('You lose') : console.log('Tied match')
 }
 
-game();
+function restartGame(){
+    playerWins = 0;
+    computerWins = 0;
+    showScores();
+}
+
+function gameEnd(){
+    if(playerWins === 5){
+        window.alert('You won!');
+        restartGame(); 
+    }
+    else if (computerWins === 5){
+        window.alert('You lost');
+        restartGame(); 
+    }
+}
+
+function playHand(){
+    computerSelection = getComputerChoice(choices);
+    opponentsPlay(computerSelection);
+    playRound(playerSelection, computerSelection);
+    showScores();
+    gameEnd();
+};
+
+const cardRock = document.getElementById('card-rock');
+const cardPaper = document.getElementById('card-paper');
+const cardScissors = document.getElementById('card-scissors');
+
+cardRock.addEventListener('click', () => {
+    document.getElementById('player-played').innerHTML = '<img src="./images/rock.jpg">';
+    playerSelection = 'rock';
+    playHand();
+});
+    
+
+cardPaper.addEventListener('click', () => {
+    document.getElementById('player-played').innerHTML = '<img src="./images/paper.jpg">';
+    playerSelection = 'paper';
+    playHand();
+});
+
+cardScissors.addEventListener('click', () => {
+    document.getElementById('player-played').innerHTML = '<img src="./images/scissors.jpg">';
+    playerSelection = 'scissors';
+    playHand();
+});
+
